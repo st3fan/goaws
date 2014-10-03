5aba0b42214368743afd564f60be4f3cb5bc9ddf
Stefan Arentz <stefan@arentz.ca>
2014-10-03 13:35:59 -0400

    Test for the ErrorResponse parser

--- aws/ses/ses_test.go
+++ aws/ses/ses_test.go
@@ -1,6 +1,9 @@
 package ses
 
-import "testing"
+import (
+	"log"
+	"testing"
+)
 
 func Test_parseListIdentitiesResponse(t *testing.T) {
 	xml := `<ListIdentitiesResponse xmlns="http://ses.amazonaws.com/doc/2010-12-01/">
@@ -19,7 +22,7 @@ func Test_parseListIdentitiesResponse(t *testing.T) {
 		t.Error("parseListIdentitiesResponse failed", err)
 	}
 
-	if response.ListIdentitiesResult.ResponseMetadata.RequestId != "cacecf23-9bf1-11e1-9279-0100e8cf109a" {
+	if response.ResponseMetadata.RequestId != "cacecf23-9bf1-11e1-9279-0100e8cf109a" {
 		t.Error("ListIdentitiesResult.ResponseMetadata.RequestId failed to parse")
 	}
 
@@ -33,5 +36,34 @@ func Test_parseListIdentitiesResponse(t *testing.T) {
 
 	if response.ListIdentitiesResult.Identities[1] != "user@example.com" {
 		t.Error("ListIdentitiesResult.Identities[1] failed to parse")
+	}
+}
+
+func Test_parseErrorResponse(t *testing.T) {
+	xml := `<ErrorResponse>
+   <Error>
+      <Type>
+         Sender
+      </Type>
+      <Code>
+         ValidationError
+      </Code>
+      <Message>
+         Value null at 'message.subject' failed to satisfy constraint: Member must not be null
+      </Message>
+   </Error>
+   <RequestId>
+      42d59b56-7407-4c4a-be0f-4c88daeea257
+   </RequestId>
+</ErrorResponse>`
+	response, err := parseErrorResponse([]byte(xml))
+	if err != nil {
+		t.Error("parseErrorResponse failed", err)
+	}
+
+	log.Printf("%+v\n", response)
+
+	if response.RequestId != "42d59b56-7407-4c4a-be0f-4c88daeea257" {
+		t.Error("response.RequestId failed to parse")
 	}
 }
